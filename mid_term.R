b46cc57d5f2b594970628749960e29ef2207a344
naveenkrishnanero <mnaveenkrishna1566@gmail.com>
2017-03-28 11:45:44 -0500

    Update mid_term.R

--- mid_term.R
+++ mid_term.R
@@ -30,13 +30,15 @@ cluster1 <- makeCluster(no_cores,type = "PSOCK")
 clusterExport(cluster1,c("si.header","img.names"))
 #clusterEvalQ(cluster1,c(library("dplyr"),library(oro.dicom)))
 si.hdrdata <- parLapply(cluster1,1:length(img.names),function(x,y=si.header,z=img.names){return(y[[z[x]]])})
-stopCluster(cluster1)             
+#stopCluster(cluster1)             
 
-cluster2 <- makeCluster(no_cores,type = "PSOCK")
-clusterExport(cluster2,c("si.header","img.names","si.hdrdata"))
+#cluster2 <- makeCluster(no_cores,type = "PSOCK")
+#clusterExport(cluster2,c("si.header","img.names","si.hdrdata"))
+clusterExport(cluster1,c("si.hdrdata"))
+
 #clusterEvalQ(cluster2,c(library("dplyr"),library(oro.dicom)))
-hdr.data <-  parLapply(cluster2,1:length(img.names),function(x,y=si.hdrdata,z=img.names){return ((rbind(y[[x]][,c("name","value")],c("image_id",z[x]))))})
-stopCluster(cluster2)
+hdr.data <-  parLapply(cluster1,1:length(img.names),function(x,y=si.hdrdata,z=img.names){return ((rbind(y[[x]][,c("name","value")],c("image_id",z[x]))))})
+#stopCluster(cluster2)
 
 
 col.list <- c("Modality","SeriesDescription","PatientID","PatientsBirthDate","SeriesNumber","ImagePositionPatient",
@@ -45,16 +47,16 @@ col.list <- c("Modality","SeriesDescription","PatientID","PatientsBirthDate","Se
 #filter(test,test[,1] %in% col.list)
 #filter for only columns required
 
-cluster3 <- makeCluster(no_cores,type = "PSOCK")
-clusterExport(cluster3,c("col.list","hdr.data"))
-clusterEvalQ(cluster3,c(library("dplyr")))
-hdr.filter.data <-  parLapply(cluster3,1:length(hdr.data),function(x,y=hdr.data,z=col.list){return(filter(y[[x]],y[[x]][,1] %in% z))})
-stopCluster(cluster3)
+#cluster3 <- makeCluster(no_cores,type = "PSOCK")
+clusterExport(cluster1,c("col.list","hdr.data"))
+clusterEvalQ(cluster1,c(library("dplyr")))
+hdr.filter.data <-  parLapply(cluster1,1:length(hdr.data),function(x,y=hdr.data,z=col.list){return(filter(y[[x]],y[[x]][,1] %in% z))})
+#stopCluster(cluster3)
 
-cluster4 <- makeCluster(no_cores,type = "PSOCK")
-clusterExport(cluster4,c("hdr.filter.data"))
-hdr.values<-parLapply(cluster4,1:length(hdr.filter.data),function(x,y=hdr.filter.data){y[[x]][,2]})
-stopCluster(cluster4)
+#cluster4 <- makeCluster(no_cores,type = "PSOCK")
+clusterExport(cluster1,c("hdr.filter.data"))
+hdr.values<-parLapply(cluster1,1:length(hdr.filter.data),function(x,y=hdr.filter.data){y[[x]][,2]})
+stopCluster(cluster1)
 
 data <- NULL
 
